5eb7f825f0a356f5e474ad1a5026458f3b0de2b2
holodnsk <holodnsk@gmail.com>
2022-12-02 13:41:37 +0700

    batch processing? save result to out dir, use original bitrate

--- sound_splitter.py
+++ sound_splitter.py
@@ -5,13 +5,14 @@ Created on Thu Sep  9 22:36:32 2021
 v.liubinas@gmail.com
 
 """
+import os
 
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import subprocess
 import sys
 
-SIZE_LIMIT = 40000
+SIZE_LIMIT = 500
 OUTPUT_FORMAT = "mp3"
 
 def seconds_to_human_readable_time(name_of_record, chunk_size_in_seconds):
@@ -59,86 +60,99 @@ def split(sound):
 def save_song(song, filename):
     song.export(
         ".//{}".format(filename),
-        bitrate = "16k",
+        # bitrate = "16k",
         format = OUTPUT_FORMAT 
     )
 
-#-------------------------------------------------------------------------
-#------------- MAIN: splits large mp3, wav files to smaller parts
-#------------- so that they can be easier uploaded in the browser applications
-#-------------------------------------------------------------------------
+print ("'Sound Splitter' splits your audio (wav, mp3, wma, ogg, aac, ...) or video (mp4, avi, flv, ...)")
+print("Be patient, this process may take a couple of minutes, depending on the length of the audio/video file.")
+for filename in sorted(os.listdir()):
 
-filename = sys.argv[-1]
-#print (filename.find(".wav",len(filename)-5, len(filename)))
-print ("'Sound Splitter' splits your audio (wav, mp3, wma, ogg, aac, ...) or video (mp4, avi, flv, ...) files into small 16 bit 16 kHz wav (mp3) sound files, so that you can use them for other purposes.")
-print ("File {} is going to be split into several separate files. \nBe patient, this process may take a couple of minutes, depending on the length of the audio/video file.\n".format(filename))
-print("Opening file {} ...".format(filename))
-if filename.find(".wav",len(filename)-5, len(filename))>0:
-    song = AudioSegment.from_wav(filename) 
-elif filename.find(".mp3",len(filename)-5, len(filename))>0:
-    song = AudioSegment.from_mp3(filename)
-elif filename.find(".ogg",len(filename)-5, len(filename))>0:
-    song = AudioSegment.from_ogg(filename) 
-elif filename.find(".flv",len(filename)-5, len(filename))>0:
-    song = AudioSegment.from_flv(filename)
-elif filename.find(".mp4",len(filename)-5, len(filename))>0:
-    song = AudioSegment.from_file(filename, "mp4")
-else:
-    song = AudioSegment.from_file(filename) 
+    #-------------------------------------------------------------------------
+    #------------- MAIN: splits large mp3, wav files to smaller parts
+    #------------- so that they can be easier uploaded in the browser applications
+    #-------------------------------------------------------------------------
+    song = AudioSegment.empty()
 
-#-- extractig filename from file path and removing the the file extension ------------------------------------
-filename_ok = True
-char = len(filename)-1
-newname=""
-while filename_ok and char>-1:
-  if filename[char] == "/" or filename[char] == '\\':
-     filename_ok = False
-  else:
-      newname = filename[char]+newname
-  char -= 1
-filename = newname[0:-4]
-#-------------------------------------------------------------------------------------------------------------
+    #print (filename.find(".wav",len(filename)-5, len(filename)))
+    if filename.find(".wav",len(filename)-5, len(filename))>0:
+        song = AudioSegment.from_wav(filename)
+    elif filename.find(".mp3",len(filename)-5, len(filename))>0:
+        song = AudioSegment.from_mp3(filename)
+    elif filename.find(".ogg",len(filename)-5, len(filename))>0:
+        song = AudioSegment.from_ogg(filename)
+    elif filename.find(".flv",len(filename)-5, len(filename))>0:
+        song = AudioSegment.from_flv(filename)
+    elif filename.find(".mp4",len(filename)-5, len(filename))>0:
+        song = AudioSegment.from_file(filename, "mp4")
+    else:
+        continue
+    print("Opening file {} ...".format(filename))
+    print(
+        "File {} is going to be split into several separate files. ".format(
+            filename))
+
+    #-- extractig filename from file path and removing the the file extension ------------------------------------
+    filename_ok = True
+    char = len(filename)-1
+    newname=""
+    while filename_ok and char>-1:
+        if filename[char] == "/" or filename[char] == '\\':
+            filename_ok = False
+        else:
+            newname = filename[char]+newname
+        char -= 1
+    filename = newname[0:-4]
+    #-------------------------------------------------------------------------------------------------------------
 
 
-print ("Total channels:", song.channels)
+    print ("Total channels:", song.channels)
 
-#no_channels = song.channels
-#for channel in range(no_channels):
-#    song = song_channels[channel].set_sample_width(2).set_frame_rate(16000)
-    
-song = song.set_channels(1).set_sample_width(2).set_frame_rate(16000)
-originalsong = song
-hours = int(song.duration_seconds // 3600)
-minutes = int(((song.duration_seconds / 3600 - song.duration_seconds // 3600) * 60)//1)
-seconds = song.duration_seconds - hours * 3600 - minutes * 60
-print ("Total record length in seconds:", round(song.duration_seconds, 3), hours, "hours", minutes, "minutes", round(seconds, 3), "seconds")
-remaining_chunk = song
-i = 1
+    #no_channels = song.channels
+    #for channel in range(no_channels):
+    #    song = song_channels[channel].set_sample_width(2).set_frame_rate(16000)
 
-size_check = 0
+    # song = song.set_channels(1).set_sample_width(2).set_frame_rate(16000)
+    originalsong = song
+    # hours = int(song.duration_seconds // 3600)
+    # minutes = int(((song.duration_seconds / 3600 - song.duration_seconds // 3600) * 60)//1)
+    # seconds = song.duration_seconds - hours * 3600 - minutes * 60
+    # print ("Total record length in seconds:", round(song.duration_seconds, 3), hours, "hours", minutes, "minutes", round(seconds, 3), "seconds")
+    remaining_chunk = song
+    i = 1
 
-while (remaining_chunk.duration_seconds > SIZE_LIMIT):    
-    chunk_count = (remaining_chunk.duration_seconds // SIZE_LIMIT) + 1
-    chunk_size = (1000 * remaining_chunk.duration_seconds) // chunk_count #takes away the number part after comma
-    interval = 2 * 60 * 1000
-    soft_cut = chunk_size - interval
-    first_chunk = remaining_chunk [:soft_cut]
-    medium_chunk = remaining_chunk [soft_cut:chunk_size]
-    
-    no_chunks, output_chunks = split (medium_chunk) 
-    for j in range(no_chunks-1):
-        first_chunk = first_chunk + output_chunks[j]
-    print(seconds_to_human_readable_time("chunk {}".format(i), len(first_chunk) / 1000))
-    remaining_chunk = remaining_chunk [len(first_chunk):]
-    #save_song(first_chunk, "{0}_channel_{1}_chunk_{2}_{3}.{4}".format(filename, channel, i, seconds_to_filename(len(first_chunk)/1000), OUTPUT_FORMAT))
-    save_song(first_chunk, "{0}_chunk_{1}_{2}.{3}".format(filename.replace(" ","_"), i, seconds_to_filename(len(first_chunk)/1000), OUTPUT_FORMAT))
-    size_check = size_check + len(first_chunk)/1000
-    i += 1
-save_song(remaining_chunk, "{0}_chunk_{1}_{2}.{3}".format(filename.replace(" ","_"), i, seconds_to_filename(len(remaining_chunk)/1000), OUTPUT_FORMAT)) 
-#save_song(remaining_chunk, "{0}_channel_{1}_chunk_{2}_{3}.{4}".format(filename, channel, i, seconds_to_filename(len(remaining_chunk)/1000), OUTPUT_FORMAT))
-print(seconds_to_human_readable_time("chunk {}".format(i), len(remaining_chunk) / 1000))
-size_check = size_check + len(remaining_chunk)/1000
-print ("  Length of all saved chunks:", round(size_check, 3))    
-save_song(originalsong, "{0}_{1}.{2}".format(filename.replace(" ","_") + "_original", seconds_to_filename(len(originalsong)/1000), OUTPUT_FORMAT))
-print(seconds_to_human_readable_time("original song in compact format", len(originalsong) / 1000))
+    size_check = 0
+
+    while (remaining_chunk.duration_seconds > SIZE_LIMIT):
+        chunk_count = (remaining_chunk.duration_seconds // SIZE_LIMIT) + 1
+        chunk_size = (1000 * remaining_chunk.duration_seconds) // chunk_count #takes away the number part after comma
+        interval = 2 * 60 * 1000
+        soft_cut = chunk_size - interval
+        first_chunk = remaining_chunk [:soft_cut]
+        medium_chunk = remaining_chunk [soft_cut:chunk_size]
+
+        no_chunks, output_chunks = split (medium_chunk)
+        for j in range(no_chunks-1):
+            first_chunk = first_chunk + output_chunks[j]
+        print(seconds_to_human_readable_time("chunk {}".format(i), len(first_chunk) / 1000))
+        remaining_chunk = remaining_chunk [len(first_chunk):]
+        #save_song(first_chunk, "{0}_channel_{1}_chunk_{2}_{3}.{4}".format(filename, channel, i, seconds_to_filename(len(first_chunk)/1000), OUTPUT_FORMAT))
+        index = str(i)
+        while len(index) < 3:
+            index = str(0)+index
+        save_song(first_chunk, "{0}-{1}.{2}".format("out\\"+filename.replace(" ","_"), index, OUTPUT_FORMAT))
+        size_check = size_check + len(first_chunk)/1000
+        i += 1
+    index = str(i)
+    while len(index) < 3:
+        index = str(0) + index
+    save_song(remaining_chunk, "{0}-{1}.{2}".format("out\\"+filename.replace(" ", "_"), index, OUTPUT_FORMAT))
+
+    #save_song(remaining_chunk, "{0}_channel_{1}_chunk_{2}_{3}.{4}".format(filename, channel, i, seconds_to_filename(len(remaining_chunk)/1000), OUTPUT_FORMAT))
+    print(seconds_to_human_readable_time("chunk {}".format(i), len(remaining_chunk) / 1000))
+    size_check = size_check + len(remaining_chunk)/1000
+    print ("  Length of all saved chunks:", round(size_check, 3))
+
+    print(seconds_to_human_readable_time("original song in compact format", len(originalsong) / 1000))
+    print()
 print("Splitting completed.")
